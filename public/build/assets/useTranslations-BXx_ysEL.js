816769b56297f43b683bf38fd8f81d3c37197958
maddhdyt <cemilcemil040704@gmail.com>
2026-08-01 11:37:09 +0700

    Force upload folder build

---
+++ public/build/assets/useTranslations-BXx_ysEL.js
@@ -0,0 +1,1 @@
+import{c as e}from"./app-B-I7hHKu.js";function i(){const{translations:r,locale:a}=e().props;return{t:(n,s={})=>{let t=r?.[n]||n;return Object.keys(s).forEach(o=>{t=t.replace(new RegExp(":"+o,"g"),s[o])}),t},locale:a}}export{i as u};
